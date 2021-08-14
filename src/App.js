27180b9a96ef0a4d0f23b75303a6b445d10b0ffe
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-08-14 16:22:42 +0600

    calling action from component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { Increment,Decrement } from './actions';
 function App() {
   const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Count value: {counter}</h1>
+      <button onClick={()=>dispatch(Increment())}>+</button>
+      <button onClick={()=>dispatch(Decrement())}>-</button>
     </div>
   );
 }
